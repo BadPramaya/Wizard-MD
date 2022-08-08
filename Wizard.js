03a5c5ddd9a0c4aeec20006487d7b2e9d4a6e73b
Neeraj <95992247+Neeraj-x0@users.noreply.github.com>
2022-08-08 20:07:54 +0530

    Update Wizard.js

--- Wizard.js
+++ Wizard.js
@@ -76,8 +76,9 @@ let tebakkalimat = db.game.kalimat = []
 let tebaklirik = db.game.lirik = []
 let tebaktebakan = db.game.tebakan = []
 let vote = db.others.vote = []
-let thumbwiz = await axios.get (global.logo, {responseType:'arraybuffer'})
+
 module.exports = Wizard = async (Wizard, m, chatUpdate, store) => {
+let thumbwiz = await axios.get (global.logo, {responseType:'arraybuffer'})
 try {
 var body = (m.mtype === 'conversation') ? m.message.conversation : (m.mtype == 'imageMessage') ? m.message.imageMessage.caption : (m.mtype == 'videoMessage') ? m.message.videoMessage.caption : (m.mtype == 'extendedTextMessage') ? m.message.extendedTextMessage.text : (m.mtype == 'buttonsResponseMessage') ? m.message.buttonsResponseMessage.selectedButtonId : (m.mtype == 'listResponseMessage') ? m.message.listResponseMessage.singleSelectReply.selectedRowId : (m.mtype == 'templateButtonReplyMessage') ? m.message.templateButtonReplyMessage.selectedId : (m.mtype === 'messageContextInfo') ? (m.message.buttonsResponseMessage?.selectedButtonId || m.message.listResponseMessage?.singleSelectReply.selectedRowId || m.text) : ''
 var budy = (typeof m.text == 'string' ? m.text : '')
